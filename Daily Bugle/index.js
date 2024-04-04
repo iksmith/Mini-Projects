ec04499e3a0d2968b6fce2bb5dddadced3cbf2f2
Kaleb <kalebjsmith@yahoo.com>
2024-04-03 21:25:41 -0500

    About 60% done with UI. Will work on finishing UI tomorrow and get into backend

--- Daily Bugle/index.js
+++ Daily Bugle/index.js
@@ -2,17 +2,21 @@ import express from "express";
 import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import path from 'path';
+
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
-app.use(`/public`, express.static(__dirname + '/public'));
+// View Engine & Directory
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// View Engine & Directory
-// app.set('view engine', 'ejs');
-// app.set('views', __dirname + '/views');
 
 // Rendering Index EJS HTML File --> Home Page
 
