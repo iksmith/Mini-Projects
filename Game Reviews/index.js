254a784a7f937cc1b9909f27ed03e7c8f129382f
Kaleb <kalebjsmith@yahoo.com>
2024-04-21 13:15:21 -0500

    Setting up Environment for game review web app.

---
+++ Game Reviews/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
+const db = new pg.Client ({
+    user : "postgres",
+    password : "1213",
+    host : "localhost",
+    port : 5432,
+    database : "game_reviews"
+}); 
+  
+// Connect Database after postgres setup 
+// db.connect();
+
+
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+// Add a closing parenthesis here
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
